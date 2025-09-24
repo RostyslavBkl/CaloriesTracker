f9876f6f689d6c9b6e45b157c05e678c60a22119
Serhii Bolkhovskyi <azeror72@gmail.com>
2025-09-24 22:14:52 +0200

    Splitted Repo logic into 2 diff folder(Interafces and Implementations). Added Enum types

--- CaloriesTracker.Server/Models/Food.cs
+++ CaloriesTracker.Server/Models/Food.cs
@@ -4,7 +4,7 @@ public class Food
 {
     public long Id { get; set; }
     public long? UserId { get; set; }
-    public string Type { get; set; }
+    public string Type { get; set; } // add Enum FoodType(api/custom)
     public string? Name { get; set; }
     public decimal? WeightG { get; set; }
     public decimal? ProteinG { get; set; }
---
+++ CaloriesTracker.Server/Repositories/IFoodRepository.cs
@@ -0,0 +1,26 @@
+﻿using CaloriesTracker.Server.Models;
+
+namespace CaloriesTracker.Server.Repositories
+{
+    public interface IFoodRepository
+    {
+        Task<Food> GetFoodByIdAsync(long Id);
+        Task<List<Food>> SearchFoodAsync(string query, long UserId);
+
+        // CRUD Custom foods
+        Task<List<Food>> GetCustomFoodsAsync(long UserId);
+        Task<Food> CreateCustomFoodAsync(Food food, long UserId);
+        Task<Food> UpdateCustomFoodAsync(Food food, long UserId);
+        Task<Food> DeleteCustomFoodAsync(long Id, long UserId);
+    }
+
+    //public class FoodRepository : IFoodRepository
+    //{
+    //    public async Task<Food> GetFoodByIdAsync(long Id)
+    //    {
+    //        return await GetFoodByIdAsync(Id);
+    //    }
+    //}
+}
+
+
