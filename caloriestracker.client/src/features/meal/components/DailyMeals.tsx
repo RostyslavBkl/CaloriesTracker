7ce099dda8ae2ef21743c5b66d4e338c7f7a5f11
Serhii Bolkhovskyi <azeror72@gmail.com>
2025-12-05 22:44:23 +0100

    Add alternative way to add Food to FoodCart through FoodInfoModal

--- caloriestracker.client/src/features/meal/components/DailyMeals.tsx
+++ caloriestracker.client/src/features/meal/components/DailyMeals.tsx
@@ -1,4 +1,4 @@
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../../../store/hooks";
 import {
   selectMealsError,
@@ -20,6 +20,7 @@ import {
   selectSearchFoodObj,
 } from "../../food/foodSelectors";
 import { CartItem, Food } from "../../food/foodType";
+import { MacronutrientsCircle } from "./MealItem/MealItemModal";
 
 const mealTypes = ["BREAKFAST", "LUNCH", "DINNER", "SNACK", "OTHER"];
 
@@ -236,7 +237,13 @@ function SearchWindow({
 
   // Early return для модалки зFood
   if (selectedFood) {
-    return <FoodInfoModal food={selectedFood} onClose={closeFoodInfo} />;
+    return (
+      <FoodInfoModal
+        food={selectedFood}
+        onClose={closeFoodInfo}
+        onAddToCart={addFoodToCart}
+      />
+    );
   }
 
   if (showСart) {
@@ -488,11 +495,36 @@ function FoodCard({
   );
 }
 
-function FoodInfoModal({ food, onClose }: { food: Food; onClose: () => void }) {
+function FoodInfoModal({
+  food,
+  onClose,
+  onAddToCart,
+}: {
+  food: Food;
+  onClose: () => void;
+  onAddToCart: (food: Food, weight: number) => void;
+}) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [weight, setWeight] = useState<number>(food.weightG || 100);
+
+  const nutrition = calculateFoodNutrition(food, weight);
+
+  useEffect(() => {
+    setWeight(food.weightG!);
+    setIsEditing(false);
+  }, [food.weightG]);
+
+  const handleBlur = () => {
+    if (weight <= 0) {
+      setWeight(food.weightG || 100);
+      return;
+    }
+    setIsEditing(false);
+    setWeight(weight);
+  };
   return (
     <div className="modal-fullscreen ">
       <div className="modal-header-fullscreen">
-        <p>{food.name}</p>
         <button className="modal-back-arrow" onClick={onClose}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 30 30">
             <path
@@ -503,6 +535,72 @@ function FoodInfoModal({ food, onClose }: { food: Food; onClose: () => void }) {
           </svg>
         </button>
       </div>
+      <div className="modal-meal-info">
+        <div className="modal-title-box">
+          <p className="item-modal-title">{food.name}</p>
+        </div>
+        <div className="modal-item-nutr-details">
+          <MacronutrientsCircle
+            protein={nutrition.proteinKcal}
+            fat={nutrition.fatKcal}
+            carbs={nutrition.carbsKcal}
+            proteinG={nutrition.actualProteinG}
+            fatG={nutrition.actualFatG}
+            carbsG={nutrition.actualCarbsG}
+          />
+        </div>
+      </div>
+      <div className="edit-weight-box">
+        <span>Weight</span>
+        <div className="edit-weight-btn">
+          {isEditing ? (
+            <input
+              autoFocus
+              type="text"
+              className="edit-weight-input"
+              value={weight}
+              onChange={(e) => setWeight(Number(e.target.value))}
+              onBlur={handleBlur}
+            />
+          ) : (
+            <span>{weight} g</span>
+          )}
+          <button className="edit-btn" onClick={() => setIsEditing(true)}>
+            <svg
+              width="24px"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M21.2799 6.40005L11.7399 15.94C10.7899 16.89 7.96987 17.33 7.33987 16.7C6.70987 16.07 7.13987 13.25 8.08987 12.3L17.6399 2.75002C17.8754 2.49308 18.1605 2.28654 18.4781 2.14284C18.7956 1.99914 19.139 1.92124 19.4875 1.9139C19.8359 1.90657 20.1823 1.96991 20.5056 2.10012C20.8289 2.23033 21.1225 2.42473 21.3686 2.67153C21.6147 2.91833 21.8083 3.21243 21.9376 3.53609C22.0669 3.85976 22.1294 4.20626 22.1211 4.55471C22.1128 4.90316 22.0339 5.24635 21.8894 5.5635C21.7448 5.88065 21.5375 6.16524 21.2799 6.40005V6.40005Z"
+                stroke="var(--muted)"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M11 4H6C4.93913 4 3.92178 4.42142 3.17163 5.17157C2.42149 5.92172 2 6.93913 2 8V18C2 19.0609 2.42149 20.0783 3.17163 20.8284C3.92178 21.5786 4.93913 22 6 22H17C19.21 22 20 20.2 20 18V13"
+                stroke="var(--muted)"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+      <button
+        onClick={() => {
+          onAddToCart(food, weight);
+          onClose();
+        }}
+        className="btn"
+        style={{ marginTop: "100px" }}
+      >
+        Add to FoodCart{" "}
+      </button>
     </div>
   );
 }
