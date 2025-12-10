0684b1f8f3a96650a672a78ab78e721f42547a7c
Serhii Bolkhovskyi <azeror72@gmail.com>
2025-12-10 21:37:32 +0100

    Reloading meals after deleting meal

--- caloriestracker.client/src/features/meal/components/DailyMeals.tsx
+++ caloriestracker.client/src/features/meal/components/DailyMeals.tsx
@@ -6,7 +6,11 @@ import {
   selectTodayMeals,
   selectTodayMealsWithSummary,
 } from "../mealSelectors";
-import { createMealWithItems } from "../mealSlices/mealSlice";
+import {
+  createMealWithItems,
+  getMealsByDay,
+  resetDeleteState,
+} from "../mealSlices/mealSlice";
 import { CreateMealInput, Meal, MealType } from "../mealTypes";
 import "../../../index.css";
 import "../../../pages/Home.css";
@@ -21,6 +25,7 @@ import {
 } from "../../food/foodSelectors";
 import { CartItem, Food } from "../../food/foodType";
 import { MacronutrientsCircle } from "./MealItem/MealItemModal";
+import { selectCurrentDiaryDay } from "../../diary/diarySelectors";
 
 const mealTypes = ["BREAKFAST", "LUNCH", "DINNER", "SNACK", "OTHER"];
 
@@ -31,8 +36,20 @@ export const DailyMeals: React.FC = () => {
   const loading = useAppSelector(selectMealsLoading);
   const error = useAppSelector(selectMealsError);
 
+  const currDD = useAppSelector(selectCurrentDiaryDay);
+
+  const isDeleted = useAppSelector((state) => state.deleteMeal.isDeleted);
+  const deleteLoading = useAppSelector((state) => state.deleteMeal.loading);
+
   const [selectedMeal, setSelectedMeal] = useState<Meal | null>(null);
   const [selectedMealType, setSelectedMealType] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (isDeleted && !deleteLoading && currDD?.diaryDayId) {
+      dispatch(getMealsByDay(currDD.diaryDayId));
+      dispatch(resetDeleteState()); // Не забудь експортувати цей action з deleteMealSlice
+    }
+  }, [isDeleted, deleteLoading, dispatch]);
 
   // const DIARY_DAY_ID = "a92573f9-1704-48fc-a261-2df6c0d10604";
 
@@ -174,7 +191,7 @@ function SearchWindow({
   // Food Card
   const [cart, setCart] = useState<CartItem[] | []>([]);
   const [showСart, setShowCart] = useState(false);
-  const selectedDate = useAppSelector(state => state.diary.selectedDate);
+  const selectedDate = useAppSelector((state) => state.diary.selectedDate);
 
   const handleCreateMeal = () => {
     if (!selectedDate) {
@@ -267,7 +284,7 @@ function SearchWindow({
     );
   }
 
-  if (showСart) { 
+  if (showСart) {
     return (
       <div className="modal-fullscreen">
         <div
@@ -738,14 +755,14 @@ function FoodCartRepresent({
               strokeLinejoin="round"
             ></g>
             <g id="SVGRepo_iconCarrier">
-            <path
-              d="M905.92 237.76a32 32 0 0 0-52.48 36.48A416 416 0 1 1 96 512a418.56 418.56 0 0 1 297.28-398.72 32 32 0 1 0-18.24-61.44A480 480 0 1 0 992 512a477.12 477.12 0 0 0-86.08-274.24z"
-              fill="currentColor"
-            />
-            <path
+              <path
+                d="M905.92 237.76a32 32 0 0 0-52.48 36.48A416 416 0 1 1 96 512a418.56 418.56 0 0 1 297.28-398.72 32 32 0 1 0-18.24-61.44A480 480 0 1 0 992 512a477.12 477.12 0 0 0-86.08-274.24z"
+                fill="currentColor"
+              />
+              <path
                 d="M630.72 113.28A413.76 413.76 0 0 1 768 185.28a32 32 0 0 0 39.68-50.24 476.8 476.8 0 0 0-160-83.2 32 32 0 0 0-18.24 61.44zM489.28 86.72a36.8 36.8 0 0 0 10.56 6.72 30.08 30.08 0 0 0 24.32 0 37.12 37.12 0 0 0 10.56-6.72A32 32 0 0 0 544 64a33.6 33.6 0 0 0-9.28-22.72A32 32 0 0 0 505.6 32a20.8 20.8 0 0 0-5.76 1.92 23.68 23.68 0 0 0-5.76 2.88l-4.8 3.84a32 32 0 0 0-6.72 10.56A32 32 0 0 0 480 64a32 32 0 0 0 2.56 12.16 37.12 37.12 0 0 0 6.72 10.56zM726.72 297.28a32 32 0 0 0-45.12 0l-169.6 169.6-169.28-169.6A32 32 0 0 0 297.6 342.4l169.28 169.6-169.6 169.28a32 32 0 1 0 45.12 45.12l169.6-169.28 169.28 169.28a32 32 0 0 0 45.12-45.12L557.12 512l169.28-169.28a32 32 0 0 0 0.32-45.44z"
-              fill="currentColor"
-            />
+                fill="currentColor"
+              />
             </g>
           </svg>
         </button>
